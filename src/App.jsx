19e93b6e6e0f0bbff2a842f9a25842193417a153
sufiyan <shaikhsufiyan174@gmail.com>
2023-05-30 18:47:27 +0530

    feature:feat-changes-done

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from './Components/Navbar/Navbar'
+// import Content from './Components/ContentPage/Content'
+
+function App() {
+  return (
+    <div>
+     <Navbar/>
+     {/* <Content/> */}
+    </div>
+  )
+}
+
+export default App
